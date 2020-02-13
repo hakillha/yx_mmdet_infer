1761bd6c38debee48352337b609909a50f2e94db
hakillha <420116830@qq.com>
2020-02-14 07:41:02 +0800

    vehicle_det

--- det_infer.py
+++ det_infer.py
@@ -4,24 +4,29 @@ import mmcv
 from glob import glob
 from os.path import join as pj
 
-img_folder = '/media/yingges/Data_Junior/data/ft_pic/all_images'
+img_folder = '/home/neut/Downloads/BITVehicle_Dataset/images/'
 CONFIG_FILES = ['/media/yingges/Data_Junior/test/12/mmdet_workdirs/cascade_rcnn_dconv_c3-c5_r100_fpn_1x_1230_p_exclude/cascade_rcnn_dconv_c3-c5_r101_fpn_1x.py',
                '/media/yingges/Data_Junior/test/12/mmdet_workdirs/cascade_rcnn_dconv_c3-c5_r100_fpn_1x_1230_p_finegrained/cascade_rcnn_dconv_c3-c5_r101_fpn_1x_p_finegrained.py']
 CHECKPOINT_FILES = ['/media/yingges/Data_Junior/test/12/mmdet_workdirs/cascade_rcnn_dconv_c3-c5_r100_fpn_1x_1230_p_exclude/epoch_31.pth',
                    '/media/yingges/Data_Junior/test/12/mmdet_workdirs/cascade_rcnn_dconv_c3-c5_r100_fpn_1x_1230_p_finegrained/epoch_24.pth']
 CONFIG_FILES_GENERIC = ['/media/yingges/Data_Junior/test/12/mmdet_workdirs/cascade_rcnn_dconv_c3-c5_r100_fpn_1x_0106_include_merged_p/cascade_rcnn_dconv_c3-c5_r101_fpn_1x.py']
 CHECKPOINT_FILES_GENERIC = ['/media/yingges/Data_Junior/test/12/mmdet_workdirs/cascade_rcnn_dconv_c3-c5_r100_fpn_1x_0106_include_merged_p/epoch_13.pth']
+CONFIG_FILES_CARS = ['/media/neut/data2/yingges/202002/model_archive/crcnn_hrnet_cars_0213/cascade_rcnn_hrnetv2p_w32_20e.py']
+CHECKPOINT_FILES_CARS = ['/media/neut/data2/ft_models/2020-02-14/log-pth/epoch_4.pth']
 global MODELS
 MODELS = None
 
-def model_init(config_files=None, checkpoint_files=None, p_generic=False):
+def model_init(task_type, config_files=None, checkpoint_files=None):
     if config_files == None and checkpoint_files == None:
-        if p_generic:
+        if task_type == 'p_generic':
             config_files = CONFIG_FILES_GENERIC
             checkpoint_files = CHECKPOINT_FILES_GENERIC
-        else:
+        elif task_type == 'p_finegrained':
             config_files = CONFIG_FILES
             checkpoint_files = CHECKPOINT_FILES
+        elif task_type == 'cars':
+            config_files = CONFIG_FILES_CARS
+            checkpoint_files = CHECKPOINT_FILES_CARS
     if not isinstance(config_files, list):
         config_files = [config_files]
     if not isinstance(checkpoint_files, list):
@@ -32,16 +37,22 @@ def model_init(config_files=None, checkpoint_files=None, p_generic=False):
         models.append(model)
     return models
 
-def single_img_test(imgs, models=None, p_generic=False):
+def single_img_test(imgs, models=None, task_type='cars'):
+    """
+        args:
+            task_type: choose from ['p_generic', 'p_finegrained', 'cars']
+    """
+
+
     # build the model from a config file and a checkpoint file
     global MODELS
     result_list = []
     if models == None:
+        # if the model hasn't been established, we can initialize it and
+        # store it in a global var so that the user doesn't have to 
+        # worry about it
         if MODELS == None:
-            if p_generic:
-                MODELS = model_init(p_generic=True)
-            else:
-                MODELS = model_init()
+            MODELS = model_init(task_type)
         models = MODELS
     if not isinstance(imgs, list):
         imgs = [imgs]
@@ -51,7 +62,9 @@ def single_img_test(imgs, models=None, p_generic=False):
             score=[],
             cls_name=[])
         for model in models:
+            print(img)
             result = inference_detector(model, img)
+            print(model.CLASSES)
             show_result(img, result, model.CLASSES, 0.5)
             for cls_id, tup in enumerate(result):
                 for res in tup:
@@ -67,4 +80,5 @@ if __name__ == '__main__':
     imgs = glob(pj(img_folder, '*'))
     # single_img_test(imgs)
     for img in imgs:
-        single_img_test(img)
+        if img.endswith('.jpg'):
+            single_img_test(img)
